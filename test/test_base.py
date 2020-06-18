4e460bdab08a3117a594001a3e2f6aff979d30b1
Fabricio Rodriguez <rodriguezfe@gmx.net>
2020-06-18 16:18:48 -0500

    Bluprints en flask

--- test/test_base.py
+++ test/test_base.py
@@ -26,11 +26,24 @@ class MainTest(TestCase):
 
         self.assert200(response)
     
-    def test_hello_post(self):
-        fake_form ={
-            "user": "fake",
-            "password": "fake-password",
-        }
-        response = self.client.post(url_for('hello', data=fake_form))
+    # def test_hello_post(self):
+    #     fake_form ={
+    #         "user": "fake",
+    #         "password": "fake-password",
+    #     }
+    #     response = self.client.post(url_for('hello', data=fake_form))
 
-        self.assertRedirects(response, url_for('index'))
+    #     self.assertRedirects(response, url_for('index'))
+
+    def test_auth_blueprint_exists(self):
+        self.assertIn('auth', self.app.blueprints)
+
+    def test_auth_login_get(self):
+        response = self.client.get(url_for('auth.login'))
+
+        self.assert200(response)
+
+    def test_auth_login_template(self):
+        self.client.get(url_for('auth.login'))
+
+        self.assertTemplateUsed('login.html')
